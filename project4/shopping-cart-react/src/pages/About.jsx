17d90c187f93c65edcd50a1ce694ff61eb6cbc8d
Jayakirupalini <118248119+Jayakirupalini@users.noreply.github.com>
2023-12-21 16:28:45 +0530

    Add files via upload

---
+++ project4/shopping-cart-react/src/pages/About.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-card">
+        <h1>About Us:</h1>
+        <p>Welcome to Furnistore, your premier destination for quality furniture that transforms houses into homes. At Furnistore, we believe that furniture is more than just a collection of pieces; it's an expression of your unique style, a reflection of your personality, and an essential part of creating spaces where memories are made.</p></div>
+
+      <div className="about-card">
+        <h1>Our Story:</h1>
+        <p>At Furnistore, our mission is to redefine your living spaces with handpicked, stylish, and functional furniture that caters to your individual taste and lifestyle. We strive to curate a diverse range of pieces that seamlessly blend modern aesthetics with timeless elegance, ensuring that every item in our collection is a testament to quality craftsmanship.</p>      </div>
+
+      <div className="about-card">
+        <h1>Our Commitment:</h1>
+        <p>We are committed to providing furniture that stands the test of time. Each piece in our collection is crafted with precision and care, using high-quality materials that ensure durability and longevity. Our dedication to quality craftsmanship means that your Furnistore furniture is not just a purchase; it's an investment in comfort, style, and lasting beauty.</p>
+
+        <p>Our commitment to exceptional customer service sets us apart. We understand that choosing the right furniture is a personal decision, and our knowledgeable and friendly team is here to assist you every step of the way. From product inquiries to delivery logistics, we strive to make your shopping experience with Furnistore as seamless and enjoyable as possible.</p>
+
+        <p>Furnistore is dedicated to promoting sustainable practices within the furniture industry. We carefully source materials from eco-friendly suppliers and prioritize responsible manufacturing processes. By choosing Furnistore, you contribute to our commitment to environmental consciousness and ethical business practices.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ project4/shopping-cart-react/src/products.js
@@ -0,0 +1,60 @@
+import product1 from "./assets/products/prod1.png";
+import product2 from "./assets/products/prod2.jpg";
+import product3 from "./assets/products/prod3.jpeg";
+import product4 from "./assets/products/prod4.jpg";
+import product5 from "./assets/products/prod5.jpg";
+import product6 from "./assets/products/prod6.jpg";
+import product7 from "./assets/products/prod7.jpg";
+import product8 from "./assets/products/prod8.jpg";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "",
+    price: 500.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "",
+    price: 600.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "",
+    price: 1000.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "",
+    price: 70.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "",
+    price: 40.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "",
+    price: 50.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "",
+    price: 70.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "",
+    price: 100.0,
+    productImage: product8,
+  },
+];
